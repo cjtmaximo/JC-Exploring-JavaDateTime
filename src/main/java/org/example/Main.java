9c641c2dd521139b9880f1e4a7293735ae3d70d0
Calvin James Maximo <calvinjames.maximo@gcash.com>
2025-08-07 17:10:26 +0800

    feat: finish Exercise 1 - `LocalDate` and `DateTimeFormatter`

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
     public static void main(String[] args) {
+        // Exercise 1 - LocalDate and DateTimeFormatter
+        LocalDate today = LocalDate.of(2025, 8, 21);
 
+        DateTimeFormatter dtf1 = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("EEEE, MMMM dd, yyyy");
+
+        System.out.println("Default format: " + today);
+        System.out.println("Custom format 1: " + today.format(dtf1));
+        System.out.println("Custom format 2: " + today.format(dtf2));
     }
 }
